21e39142b8e5f95bc3c902671ddadf6b685acb22
109project-deeplearning <109project.deeplearning@gmail.com>
2023-04-16 01:00:59 +0800

    item 新增/更新/刪減

--- itemRequest.php
+++ itemRequest.php
@@ -12,7 +12,7 @@ class itemRequest{
             $method_Func = $method.'Func';
             return self::$method_Func($_REQUEST);
         }else{
-            return array("傳入格式錯誤", 403, 'Fail');
+            return array("傳入格式錯誤", 400, 'Fail');
         }
     }
 
@@ -21,7 +21,7 @@ class itemRequest{
         $body = json_decode(file_get_contents('php://input'), true);
         if(Utility::checkIsValidData(['item', 'offset', 'merchantUid'], $body)){
             $item = $body['item'];
-            $offset = $body['offset'];
+            $offset = $body['offset'][0].','.$body['offset'][1];
             $merchantUid = $body['merchantUid'];
 
             // create
@@ -34,41 +34,54 @@ class itemRequest{
             $row = mysqli_fetch_array($data);
             # return array("創建成功", 200, "Success");
             return array($row['uid'], 200, "Success");
+        }else{
+            return array("漏填必填", 401, 'Fail');
         }
     }
 
     // PUT
     private static function putFunc(){
-        $body = jaon_decode(file_get_context('php://input'), True);
+        $body = json_decode(file_get_contents('php://input'), True);
         if(Utility::checkIsValidData(['uid', 'item', 'offset', 'merchantUid'], $body)){
             $uid = $body['uid'];
             $item = $body['item'];
-            $offset = $body['offset'];
+            $offset = $body['offset'][0].','.$body['offset'][1];
             $merchantUid = $body['merchantUid'];
 
-            $sql_query = "UPDATE item SET item = '$item', offset = '$offset', merchantUid = '$merchantUid";
+            // item is not exist
+            $sql_query = "SELECT * FROM item WHERE uid = '$uid'";
+            $data = MysqlUtility::MysqlQuery($sql_query);
+            if(mysqli_num_rows($data) == 0){
+                return array("查無資料", 403, "Fail");
+            }
+
+            $sql_query = "UPDATE item SET item = '$item', offset = '$offset', merchantUid = '$merchantUid'  WHERE uid = '$uid'";
             MysqlUtility::MysqlQuery($sql_query);
 
             return array("更新成功", 200, "Success");
+        }else{
+            return array("漏填必填", 401, 'Fail');
         }
     }
 
     // DELETE
     private static function deleteFunc(){
-        $body = jaon_decode(file_get_context('php://input'), True);
+        $body = json_decode(file_get_contents('php://input'), True);
         if(Utility::checkIsValidData(['uid'], $body)){
             $uid = $body['uid'];
     
             $sql_query = "SELECT * FROM item WHERE uid = '$uid'";
             $data = MysqlUtility::MysqlQuery($sql_query);
             if(mysqli_num_rows($data) == 0){
-                return array("該物件不存在", 401, "Input Wrong");
+                return array("該物件不存在", 403, "Input Wrong");
             }
 
             $sql_query = "DELETE FROM item WHERE uid = '$uid'";
             MysqlUtility::MysqlQuery($sql_query);
 
             return array("刪除成功", 200, "Success");
+        }else{
+            return array("漏填必填", 401, 'Fail');
         }
     }
 }
