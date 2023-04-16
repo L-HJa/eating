bc165f277e3a35f6998e385882a9ed1b1ef0d814
109project-deeplearning <109project.deeplearning@gmail.com>
2023-04-16 19:49:57 +0800

    put/delete

--- photoRequest.php
+++ photoRequest.php
@@ -1,71 +1,66 @@
 <?php
 
 require('connectMYSQL.php');
+require_once('Utility.php');
 
 class photoRequest{
-    private static $method_type = array("post", "put");
+    private static $method_type = array("put", "delete");
     private static $allowed_type = array("jpg", "jepg", "png");
 
     public static function getRequest(){
         $method = strtolower($_SERVER['REQUEST_METHOD']);
         if(in_array($method, self::$method_type)){
-            $method_func = $method.'Func';
-            return self::$method_func($_REQUEST);
+            $method_Func = $method.'Func';
+            return self::$method_Func($_REQUEST);
         }else{ 
-            return array("傳入格式錯誤", 400, 'FailSignin');
-        }
-    }
-
-    private static function postFunc(){
-        $body = json_decode(file_get_contents('php://input'), true);
-        if(!(empty($body['uid']) || empty($body['role']))){
-            $uid = $body['uid'];
-            $role = $body['role'];
-            $sql_query = "SELECT * FROM $role WHERE uid = '$uid'";
-            $data = MysqlUtility::MysqlQuery($sql_query);
-            $row = mysqli_fetch_array($data, MYSQLI_ASSOC);
-            $ret['photo'] =  $row['photo'];
-            return array($ret, 200, 'GetPhoto');
-        }else{
-            return array("漏填必填", 401, 'FailGetPhoto');
+            // return"is not post";
+            return array("傳入格式錯誤", 403, 'Fail');
         }
     }
 
     private static function putFunc(){
         $body = json_decode(file_get_contents('php://input'), true);
-        if(!(empty($body['email']) || empty($body['password']) || empty($body['image']) || empty($body['role']))){
-            // encode   存DB
-            $email = $body['email'];
-            $password = $body['password'];
+        if(Utility::checkIsValidData(['uid', 'image', 'role'])){
+            $uid = $body['uid'];
             $photo = $body['photo'];
             $role = $body['role'];
 
-            $sql_findHashPassword = "SELECT * FROM $role WHERE email = '$email'";
+            $sql_findHashPassword = "SELECT * FROM $role WHERE uid = '$uid'";
             $data = MysqlUtility::MysqlQuery($sql_findHashPassword);
 
-            // do not exist
-            if(mysqli_num_rows($data) == 0){
-                // $Response_data = array('account or password is wrong!'); 
-                return array("帳密錯誤", 403, 'FailLogin');  // email
+            if(mysqli_num_rows($data) == 0){    // 有該帳號
+                $sql_query = "UPDATE $role SET  photo = '$photo' WHERE uid = '$uid'";
+                $data = MysqlUtility::MysqlQuery($sql_query);
+                return array("更新成功", 200, 'Success');
+            }else{  // 無該帳號
+                return array("無此帳號", 403, 'Fail');  
             }
             
-            $hash = mysqli_fetch_array($data, MYSQLI_ASSOC);
-            if(password_verify($password, $hash['password'])){
-                $sql_query = "UPDATE $role SET  photo = '$photo' WHERE email = '$email'";
-                $data = MysqlUtility::MysqlQuery($sql_query);
-                return array("Success", 200, 'Success');
-                
-            }else{
-                // $Response_data = "account or password is wrong.";
-                return array("帳密錯誤", 403, 'FailLogin');   // password
-            }
-
         }else{
-            return array("id, image 為必填項目", 401, 'FailSignin');
+            return array("漏填必填", 401, 'Fail');  // ['uid', 'image', 'role']
         }
-
     }
 
+    private static function deleteFunc(){
+        $body = json_decode(file_get_contents('php://input'), true);
+        if(Utility::checkIsValidData(['uid', 'role'], $body)){
+            $uid = $body['uid'];
+            $role =  $body['role'];
+            
+            $sql_query = "SELECT * FROM $role WHERE uid = $uid";
+            $data = MysqlUtility::MysqlQuery($sql_query);
+            
+            if(mysqli_num_rows($data) != 0){    // 有該帳號
+                $sql_query = "UPDATE $role SET photo = '' WHERE uid = $uid";
+                MysqlUtility::MysqlQuery($sql_query);
+                return array("刪除成功", 200, 'Success');
+            }else{  // 無該帳號
+                return array("無該帳號", 403, 'Fail');
+            }
+        }else{
+            return array("漏填必填", 401, "Fail"); // ['uid', 'role']
+        }
+    }
 }
 
 ?>
