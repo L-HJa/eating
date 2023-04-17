d3882221018883cdb457c9bc5670cfc357cd81c4
109project-deeplearning <109project.deeplearning@gmail.com>
2023-04-17 19:45:06 +0800

    customer

--- userLogin.php
+++ userLogin.php
@@ -6,6 +6,7 @@ require_once('utility.php');
 class loginRequest{
     private static $method_type = array('post'); 
 
+    // merchant -------------------------------------------------------------------
     public static function getRequest(){
         $method = strtolower($_SERVER['REQUEST_METHOD']);
         if(in_array($method, self::$method_type)){
@@ -17,7 +18,7 @@ class loginRequest{
         }
     }
 
-    // POST 登入 -------------------------------------------------------------------
+    // POST 
     private static function postFunc(){
         $body = json_decode(file_get_contents('php://input'), true);
         if(Utility::checkIsValidData(['email', 'password', 'role'], $body)) {
@@ -44,6 +45,47 @@ class loginRequest{
 
         }else{
             // email, password role 為必填項目
+            return array("缺少必要資料", 401, 'Fail');
+        } 
+    }
+
+
+
+    // customer -------------------------------------------------------------------
+    public static function getRequest_cus(){
+        $method = strtolower($_SERVER['REQUEST_METHOD']);
+        if(in_array($method, self::$method_type)){
+            $method_Func = $method.'Func_cus';
+            return self::$method_Func($_REQUEST);
+        }else{ 
+            // return"is not post";
+            return array("傳入格式錯誤", 400, 'FailSignin');
+        }
+    }
+
+    // POST 
+    private static function postFunc_cus(){
+        $body = json_decode(file_get_contents('php://input'), true);
+        if(Utility::checkIsValidData(['email', 'password'], $body)) {
+            $email = $body['email'];
+            $password = $body['password'];
+
+            $sql_findHashPassword = "SELECT * FROM customer WHERE email = '$email'";
+            $data = MysqlUtility::MysqlQuery($sql_findHashPassword);
+
+            // 存在&帳密正確
+            if(mysqli_num_rows($data) > 0){
+                $row = mysqli_fetch_array($data, MYSQLI_ASSOC);
+                if(password_verify($password, $row['password'])){
+                    // 回傳所有個人資訊
+                    return array($row, 200, 'Success');
+                }
+            }
+
+            return array("帳密錯誤", 403, 'Fail');   
+
+        }else{
+            // email, password 為必填項目
             return array("缺少必要資料", 401, 'Fail');
         } 
     }
