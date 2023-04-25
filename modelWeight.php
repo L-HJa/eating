e808e7b7c6076cba26b961713e6aed95ac45e6f9
109project-deeplearning <109project.deeplearning@gmail.com>
2023-04-25 22:22:52 +0800

    新增查詢已上傳資料以及刪除已上傳資料

--- modelWeight.php
+++ modelWeight.php
@@ -60,8 +60,8 @@ class modelWeight {
     // 訓練目標檢測模型
     static public function trainObjectDetectionModel() {
         $body = json_decode(file_get_contents('php://input'), true);
-        if(Utility::checkIsValidData(["uid"], $body)) {
-            $uid = $body["uid"];
+        if(Utility::checkIsValidData(["merchantUid"], $body)) {
+            $uid = $body["merchantUid"];
             $root = self::$storageRoot;
 
             if(!Utility::checkIsMerchantExist($uid)) {
@@ -74,5 +74,97 @@ class modelWeight {
         } else {
             return array("缺少必要資料", 403, "Fail");
         }
+    }
+
+    // 獲取總共有多少訓練圖像
+    static public function objectDetectionTrainImageCount() {
+        $body = json_decode(file_get_contents('php://input'), true);
+        if(Utility::checkIsValidData(["merchantUid"], $body)) {
+            $uid = $body["merchantUid"];
+            if(!Utility::checkIsMerchantExist($uid)) {
+                return array("商家不存在", 404, "Fail");
+            }
+            $dir = self::$storageRoot."/$uid"."/ObjectDetection"."/imgs";
+            if(is_dir($dir)) {
+                $folderInfo = scandir($dir);
+                $fileCount = strval(count($folderInfo) - 2);
+                return array($fileCount, 200, "Success");
+            } else {
+                return array("資料夾狀態錯誤", 500, "Fail");
+            }
+        } else {
+            return array("缺少必要資料", 403, "Fail");
+        }
+    }
+
+    // 獲取已上傳的圖像資料
+    static public function objectDetectionTrainImageInfo() {
+        $body = json_decode(file_get_contents('php://input'), true);
+        if(Utility::checkIsValidData(["merchantUid", "page", "gap"], $body)) {
+            $uid = $body["merchantUid"];
+            $page = $body["page"];
+            $gap = $body["gap"];
+            if(!Utility::checkIsMerchantExist($uid)) {
+                return array("商家不存在", 404, "Fail");
+            } elseif($page < 1) {
+                return array("查詢頁面不可小於1", 401, "Fail");
+            }
+            $rootDir = self::$storageRoot."/$uid"."/ObjectDetection";
+            $imageDir = $rootDir."/imgs";
+            $annoDir = $rootDir."/annotations";
+            $imageFolderInfo = scandir($imageDir);
+            $annoFolderInfo = scandir($annoDir);
+            $imageFolderInfo = array_slice($imageFolderInfo, 2);
+            $annoFolderInfo = array_slice($annoFolderInfo, 2);
+            sort($imageFolderInfo);
+            sort($annoFolderInfo);
+            $targetImageFilesName = array_slice($imageFolderInfo, $gap * ($page - 1), $gap);
+            $targetAnnoFilesName = array_slice($annoFolderInfo, $gap * ($page - 1), $gap);
+            $fileCount = strval(count($targetImageFilesName));
+            $result = array();
+            for($i=0;$i<$fileCount;$i++) {
+                $imagePath = $rootDir."/imgs"."/$targetImageFilesName[$i]";
+                $annoPath = $rootDir."/annotations"."/$targetAnnoFilesName[$i]";
+                $imageData = file_get_contents($imagePath);
+                $base64 = base64_encode($imageData);
+                $result[$i]["image"] = $base64;
+                $annoData = file_get_contents($annoPath);
+                $result[$i]["anno"] = $annoData;
+            }
+            return array($result, 200, "Success");
+        } else {
+            return array("缺少必要資料", 403, "Fail");
+        }
+    }
+
+    // 移除目標檢測訓練圖像資料
+    static public function deleteObjectDetectionTrainImage() {
+        $body = json_decode(file_get_contents('php://input'), true);
+        if(Utility::checkIsValidData(["merchantUid", "target"], $body)) {
+            $uid = $body["merchantUid"];
+            $target = $body["target"];
+
+            $rootPath = self::$storageRoot."/$uid"."/ObjectDetection";
+            $imagePath = $rootPath."/imgs"."/$target.jpg";
+            $annoPath = $rootPath."/annotations"."/$target.txt";
+            if(file_exists($imagePath)) {
+                unlink($imagePath);
+            } else {
+                return array("檔案不存在", 404, "Not Found Data");
+            }
+            if(file_exists($annoPath)) {
+                unlink($annoPath);
+            } else {
+                return array("檔案不存在", 404, "Not Found Data");
+            }
+            return array("Success", 200, "Success");
+        } else {
+            return array("缺少必要資料", 403, "Fail");
+        }
+    }
+
+    // 檢查圖像資料數量足夠
+    static private function checkPhotoIsManyEnough() {
+
     }
 }
