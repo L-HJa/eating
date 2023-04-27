394705545ed6bcb2996ba95f65f21722cec616fe
109project-deeplearning <109project.deeplearning@gmail.com>
2023-04-28 02:48:26 +0800

    customer 更改/查看喜愛清單, 搜尋店家資料

--- userRequest.php
+++ userRequest.php
@@ -88,19 +88,27 @@ class userRequest{
 
 
 
-    // customer  -------------------------------------------------------------------
-    public static function getRequest_cus(){
+
+
+    
+
+
+}
+
+class customerRequest{
+    private static $method_type = array('post', 'put', 'delete');
+    
+    public static function getRequest(){
         $method = strtolower($_SERVER['REQUEST_METHOD']);
         if(in_array($method, self::$method_type)){
-            $method_Func = $method.'Func_cus';
+            $method_Func = $method.'Func';
             return self::$method_Func($_REQUEST);
         }else{
-            return array("傳入格式錯誤", 400, 'FailSignin');
+            return array("傳入格式錯誤", 400, "Fail");
         }
     }
-
     // PUT Update
-    private static function putFunc_cus(){
+    private static function putFunc(){
         $body = json_decode(file_get_contents('php://input'), true);
 
         if(Utility::checkIsValidData(['uid'], $body)){
@@ -144,7 +152,7 @@ class userRequest{
     } 
 
     // Delete
-    private static function deleteFunc_cus(){
+    private static function deleteFunc(){
         $body = json_decode(file_get_contents('php://input'), true);
         if(Utility::checkIsValidData(['uid'], $body)){
             $uid = $body['uid'];
@@ -165,12 +173,15 @@ class userRequest{
         }
     }
 
+}
 
+class customerGetMerchantInfo{
     // post 獲得關鍵字相關的店家資訊
-    public static function getFunc_cus_keyName(){
+    public static function keyName(){
         $body = json_decode(file_get_contents('php://input'), true);
-        if(Utility::checkIsValidData(['name'], $body)){
+        if(Utility::checkIsValidData(['name', 'customerUid'], $body)){
             $name = $body['name'];
+            $customerUid = $body['customerUid'];
             $sql_query = "SELECT * FROM merchant  WHERE name LIKE '%$name%' ORDER BY uid";
             $data = MysqlUtility::MysqlQuery($sql_query);
             $result = array();
@@ -179,7 +190,11 @@ class userRequest{
             if($numOfMerchant == 0) return array("無此名稱的店家", 201, "Success");
             while($numOfMerchant != 0){
                 $row = mysqli_fetch_array($data);
+                $merchantUid = $row['uid'];
                 $result[$cnt] = array( "uid" => $row['uid'], "name" => $row['name'], "photo" => $row['photo'], "location" => $row['location']); 
+                $favorite_sqlQuery = "SELECT * FROM favorite WHERE customerUid = $customerUid AND merchantUid = $merchantUid";
+                $favorite_data = MysqlUtility::MysqlQuery($favorite_sqlQuery);
+                $result[$cnt]['favorite'] = (mysqli_num_rows($favorite_data) != 0);
                 $numOfMerchant -= 1;
                 $cnt += 1;
             }
@@ -189,7 +204,7 @@ class userRequest{
         }
     }
     // post 獲得指定店家的資訊
-    public static function getFunc_cus_getDetails(){
+    public static function getDetails(){
         $body = json_decode(file_get_contents('php://input'), true);
         if(Utility::checkIsValidData(['uid'], $body)){
             $uid = $body['uid'];
@@ -208,8 +223,80 @@ class userRequest{
             return array("缺少必要資料", 401, "Fail");
         }
     }
-
-
 }
 
+class customerFavorite{
+    private static $method_type = array('post', 'put', 'delete');
+    
+    public static function getRequest(){
+        $method = strtolower($_SERVER['REQUEST_METHOD']);
+        if(in_array($method, self::$method_type)){
+            $method_Func = $method.'Func';
+            return self::$method_Func($_REQUEST);
+        }else{
+            return array("傳入格式錯誤", 400, "Fail");
+        }
+    }
+    // POST 獲取
+    private static function postFunc(){
+        $body = json_decode(file_get_contents('php://input'), True);
+        if(Utility::checkIsValidData(['customerUid'], $body)){
+            $customerUid = $body['customerUid'];
+            $favorite_sqlquery = "SELECT * FROM favorite WHERE customerUid = '$customerUid'";
+            $favorite_data = MysqlUtility::MysqlQuery($favorite_sqlquery);
+            $numOfFData = mysqli_num_rows($favorite_data);
+            if($numOfFData == 0) return array("尚未新增喜愛店家", 201, "Success");
+            $cnt = 0;
+            $result = array();
+            while($numOfFData != 0){
+                $favorite_row = mysqli_fetch_array($favorite_data);
+                $merchantUid = $favorite_row['merchantUid'];
+                $merchantInfo_sqlQuery = "SELECT * FROM merchant WHERE uid = $merchantUid";
+                $merchantInfo_data = MysqlUtility::MysqlQuery($merchantInfo_sqlQuery);
+                $merchantInfo_row = mysqli_fetch_array($merchantInfo_data);
+                $result[$cnt] = array("uid" => $merchantInfo_row['uid'], "name" => $merchantInfo_row['name'], "location" => $merchantInfo_row['location'], "photo" => $merchantInfo_row['photo']);
+                $numOfFData -= 1;
+                $cnt += 1;
+            } 
+            return array($result, 200, "Success");
+        }else{
+            return array("缺少必要資料", 401, "Fail");
+        }
+    }
+    // PUT 新增
+    private static function putFunc(){
+        $body = json_decode(file_get_contents('php://input'), True);
+        if(Utility::checkIsValidData(['customerUid', 'merchantUid'], $body)){
+            $customerUid = $body['customerUid'];
+            $merchantUid = $body['merchantUid'];
+            // 無此店家
+            $check_sqlQuery = "SELECT * FROM merchant WHERE uid = $merchantUid";
+            $check_data = MysqlUtility::MysqlQuery($check_sqlQuery);
+            if(mysqli_num_rows($check_data) == 0)   return array("無此店家", 403, "Fail");
+
+            $check_sqlQuery = "SELECT * FROM favorite WHERE merchantUid = $merchantUid AND customerUid = $customerUid";
+            $check_data = MysqlUtility::MysqlQuery($check_sqlQuery);
+            if(mysqli_num_rows($check_data) == 0){
+                $sql_query = "INSERT INTO favorite (customerUid, merchantUid) VALUES ('$customerUid', '$merchantUid')";
+                MysqlUtility::MysqlQuery($sql_query);
+            }
+            return array("更新成功.", 200, 'Success');    
+        }else{
+            return array("缺少必要資料", 401, "Fail");
+        }
+    }
+    // DELETE 刪除
+    private static function deleteFunc(){
+        $body = json_decode(file_get_contents('php://input'), True);
+        if(Utility::checkIsValidData(['customerUid', 'merchantUid'], $body)){
+            $customerUid = $body['customerUid'];
+            $merchantUid = $body['merchantUid'];
+            $sql_query = "DELETE FROM favorite WHERE customerUid = '$customerUid'AND merchantUid = '$merchantUid'";
+            MysqlUtility::MysqlQuery($sql_query);
+            return array("刪除成功", 200, 'Success');
+        }else{
+            return array("缺少必要資料", 401, "Fail");
+        }
+    }
+}
 ?>
